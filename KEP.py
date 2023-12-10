97382ab48f10a18b62aaf8ba097424f9737964a5
plofknaapje <robinverhoef1007@gmail.com>
2023-12-10 14:32:54 +0100

    Simple KEP implementation

---
+++ KEP.py
@@ -0,0 +1,107 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import gurobipy as gp
+from gurobipy import GRB
+
+def generate_nodes(n: int, p: float, k=1, seed=42) -> tuple:
+    graph = nx.fast_gnp_random_graph(n=n, p=p, seed=seed, directed=True)
+    return list(graph.nodes), list(graph.edges)
+
+nodes, edges = generate_nodes(20, 0.1)
+
+def simple_KEP(nodes, edges):
+    try:
+        m = gp.Model("KEP")
+        m_edges = m.addVars(edges, vtype=GRB.BINARY, name="t")
+        m_nodes = m.addVars(nodes, vtype=GRB.BINARY, name="p")
+
+        m.setObjective(m_edges.sum(), GRB.MAXIMIZE)
+
+        m.addConstrs(
+            gp.quicksum(m_edges[i, o] for i, o in edges if i == node) == m_nodes[node]
+            for node in nodes)
+
+        m.addConstrs(
+            gp.quicksum(m_edges[i, o] for i, o in edges if o == node) == m_nodes[node]
+            for node in nodes)
+
+        m.optimize()
+
+        for v in m.getVars():
+            print(f"{v.VarName} {v.X:g}")
+
+        print(f"Obj: {m.ObjVal:g}")
+
+        transplants = [edge for edge, v in m_edges.items() if v.X == 1]
+
+        return transplants
+
+    except gp.GurobiError as e:
+        print(f"Error code {e.errno}: {e}")
+
+    except AttributeError:
+        print("Encountered an attribute error")
+
+def cycle_KEP(nodes, edges, max_cycle):
+    G = nx.DiGraph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+    cycles = [tuple(cycle) for cycle in nx.simple_cycles(G, max_cycle)]
+
+    try:
+        m = gp.Model("cycle KEP")
+        m_cycles = m.addVars(cycles, vtype=GRB.BINARY, name="c")
+        m_nodes = m.addVars(nodes, vtype=GRB.BINARY, name="p")
+
+        m.setObjective(gp.quicksum(len(cycle) * m_cycles[cycle]
+                    for cycle in cycles), GRB.MAXIMIZE)
+
+        m.addConstrs(
+            gp.quicksum(m_cycles[cycle] for cycle in cycles if node in cycle) == m_nodes[node]
+            for node in nodes
+        )
+
+        m.optimize()
+
+        for v in m.getVars():
+            print(f"{v.VarName} {v.X:g}")
+
+        print(f"Obj: {m.ObjVal:g}")
+
+        cycles = [cycle for cycle, v in m_cycles.items() if v.X == 1]
+
+        return cycles
+
+    except gp.GurobiError as e:
+        print(f"Error code {e.errno}: {e}")
+
+    except AttributeError:
+        print("Encountered an attribute error")
+
+
+transplants = simple_KEP(nodes, edges)
+
+unused_edges = [edge for edge in edges if edge not in transplants]
+G = nx.DiGraph()
+G.add_nodes_from(nodes)
+G.add_edges_from(edges)
+pos = nx.spring_layout(G, seed=1)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos, edgelist=unused_edges)
+nx.draw_networkx_edges(G, pos, edgelist=transplants, width=2, edge_color="red")
+plt.show()
+
+cycles = cycle_KEP(nodes, edges, 5)
+print(cycles)
+
+nx.draw_networkx_nodes(G, pos)
+for cycle in cycles:
+    edges = []
+    i = cycle[-1]
+    width = 1
+    for j in cycle:
+        edges.append([i, j])
+        i = j
+    nx.draw_networkx_edges(G, pos, edgelist=edges, width=width)
+    width += 0.2
+plt.show()
