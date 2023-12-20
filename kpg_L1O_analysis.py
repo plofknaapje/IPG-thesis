92e7b6637d8af5bd3c40a8c886873920e5b62e87
robnvuurdraak <robinverhoef1007@gmail.com>
2023-12-20 13:16:58 +0100

    more info

--- kpg_L1O_analysis.py
+++ kpg_L1O_analysis.py
@@ -11,13 +11,15 @@ results = {}
 
 # Run the problem while ignoring one item each time.
 for i in range(master_kpg.m):
+    # Only consider items which were used in the master solution.
     if sum(master_result.X[:, i]) > 0:
         payoffs = master_kpg.payoffs.copy()
         payoffs[:, i] = 0
         interaction_coefs = master_kpg.interaction_coefs.copy()
         interaction_coefs[:, :, i] = 0
         weights = master_kpg.weights
-        instance = kpg.KPG(weights, payoffs, interaction_coefs)
+        # New problem where the item i is ignored (payoff and interaction = 0)
+        instance = kpg.KPG(weights, payoffs, interaction_coefs, capacity = master_kpg.capacity)
         instance.capacity = master_kpg.capacity
         results[i] = (kpg.zero_regrets(instance))
         print(results[i].ObjVal)
