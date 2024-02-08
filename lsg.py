d98c204998d2687d697691193c3171ee8faaaf6d
robnvuurdraak <robinverhoef1007@gmail.com>
2024-02-08 15:38:51 +0100

    Further work

--- lsg.py
+++ lsg.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import random
+from statistics import mean 
 
 @dataclass
 class LSG:
@@ -9,9 +11,7 @@ class LSG:
     customer_locs: list # J
     retail_locs: list # K
     potential_locs: dict # K_i \subset K for i \in I
-    retail_strats: dict # i \in I
-    A: list # possible parameters for alpha
-    B: list # possible parameters for beta
+    # retail_strats: dict # i \in I
 
     # Exogenous parameters
     pop_count: list # (j)
@@ -19,16 +19,81 @@ class LSG:
     costs: dict # (i, k)
     max_dist: int
     distance: dict # (j, k)
-    convenience: list # (k)
+    norm_distance: dict # (j, k)
+    conveniences: list # (k)
     max_convenience: int
-    observed_locs: list # (i)
+    # observed_locs: list # (i)
 
     # Endogenous variables
     utility: dict # (i, j, k)
     alpha: int # normalised sensitivity to distance
-    beta: list # (i) brand attractiveness
-    delta: list # (i) unilateral imporvement potentials
+    betas: list # (i) brand attractiveness
+    # delta: list # (i) unilateral imporvement potentials
 
-    def patreons(i, j):
-        return 0
+    def patreons(self, i: int, j: int, x: list) -> float:
+        cum_util_i = 0
+        for k in self.potential_locs[i]:
+            if self.distance[j, k] <= self.max_dist:
+                cum_util_i += x[i, k] * self.utility[i, j, k]
+        
+        cum_util = 0
+        for ii in self.incumbents:
+            for k in self.retail_locs:
+                cum_util += x[ii, k] * self.utility[ii, j, k]
+        
+        return cum_util_i / cum_util
 
+
+def random_LSG(incumbents: int, customers: int, locations: int, seed: int = 42) -> LSG:
+    random.seed(seed)
+
+    # Base parameters
+    i = list(range(incumbents))
+    j = list(range(customers))
+    k = list(range(locations))
+
+    # Potential location assignment
+    potential = {ii: [] for ii in i}
+    for kk in k:
+        potential[random.choice(i)].append(kk)
+    
+    population = [random.randint(45, 55) for _ in j]
+    margin = {(ii,jj): 1 for ii in i for jj in j}
+    costs = {(ii, kk): random.randint(45, 56) for ii in i for kk in k}
+    max_dist = 100
+
+    pop_coords = [(random.randint(1, 100), random.randint(1, 100)) for _ in j]
+    store_coords = [(random.randint(1, 100), random.randint(1, 100)) for _ in k]
+
+    # Distance between stores and customers
+    distance = {}
+    norm_distance = {}
+    for jj, pop in enumerate(pop_coords):
+        for kk, store in enumerate(store_coords):
+            # Manhattan distance
+            distance[jj, kk] = abs(pop[0] - store[0]) + abs(pop[1] - store[1])
+            norm_distance[jj, kk] = (max_dist - distance[jj, kk]) / max_dist
+    
+    competition_coords = [(random.randint(1, 100), random.randint(1, 100)) for _ in range(50)]
+    conveniences = []
+    for kk, store in enumerate(store_coords):
+        comp_dists = []
+        for comp in competition_coords:
+            comp_dists.append(abs(store[0] - comp[0]) + abs(store[1] - comp[1]))
+        conveniences.append(mean(comp_dists))
+    
+    max_convenience = max(conveniences)
+    
+    for cc, conv in enumerate(conveniences):
+        conveniences[cc] = (max_convenience - conv) / max_convenience
+
+    alpha = 0.4
+    betas = [random.random() for _ in i]
+
+    utility = {(ii, jj, kk): betas[ii] + alpha * distance[jj, kk] + (1 - alpha) * conveniences[kk]
+                   for ii in i for jj in j for kk in k}
+
+    return LSG(i, j, k, potential, population, margin, costs, max_dist, distance, 
+               norm_distance, conveniences, max_convenience, utility, alpha, betas)
+
+example = random_LSG(2, 10, 10)
