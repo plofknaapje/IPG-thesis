5ca6fb5e67152dba8d78f638b22cc58330c170c1
robnvuurdraak <robinverhoef1007@gmail.com>
2024-04-18 15:38:06 +0200

    local inverse of KPG

--- src/local_inverse_kpg_test.py
+++ src/local_inverse_kpg_test.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-from problems.knapsack_packing_game import generate_random_KPG
-from methods.local_inverse_kpg import local_inverse_weights
+from methods.inverse_kpg import generate_weight_problems
+from methods.local_inverse_kpg import local_inverse_weights, local_inverse_payoffs
 
-instance = generate_random_KPG()
+rng = np.random.default_rng(1)
 
-solution = instance.solve_greedy()
-print(solution)
+for _ in range(10):
+    instance = generate_weight_problems(1, 2, 25, neg_inter=True, rng=rng, solve=False)[
+        0
+    ]
 
-new_weights = local_inverse_weights(instance)
-print(np.abs(new_weights - instance.weights).sum())
+    new_weights = local_inverse_weights(instance)
+    print(np.abs(new_weights - instance.weights))
+    print(np.abs(new_weights - instance.weights).sum())
+
+    new_payoffs = local_inverse_payoffs(instance)
+    print(np.abs(new_payoffs - instance.payoffs))
+    print(np.abs(new_payoffs - instance.payoffs).sum())
--- src/methods/local_inverse_kpg.py
+++ src/methods/local_inverse_kpg.py
@@ -9,7 +9,7 @@ from problems.knapsack_packing_game import KnapsackPackingGame
 eps = 0.001
 
 
-def local_inverse_weights(problem: KnapsackPackingGame, verbose=False) -> np.ndarray:
+def local_inverse_weights(problem: KnapsackPackingGame) -> np.ndarray:
     greedy_solution = problem.solve_greedy()
     i_range = list(range(problem.m))
 
@@ -37,7 +37,7 @@ def local_inverse_weights(problem: KnapsackPackingGame, verbose=False) -> np.nda
 
     model.optimize()
     if model.Status == GRB.INFEASIBLE:
-        raise ValueError("Problem is Infeasible")
+        raise ValueError("Problem is initially Infeasible")
 
     while True:
         new_constraint = False
@@ -48,8 +48,10 @@ def local_inverse_weights(problem: KnapsackPackingGame, verbose=False) -> np.nda
             )
 
             if (
-                problem.obj_value(p, greedy_solution, new_player_x)
-                >= problem.obj_value(p, greedy_solution) + eps
+                problem.obj_value(
+                    p, solution=greedy_solution, player_solution=new_player_x
+                )
+                >= problem.obj_value(p, solution=greedy_solution) + eps
             ):
                 model.addConstr(new_player_x @ w[p] >= problem.capacity[p] + eps)
                 new_constraint = True
@@ -64,3 +66,65 @@ def local_inverse_weights(problem: KnapsackPackingGame, verbose=False) -> np.nda
     return w.X.astype(int)
 
 
+def local_inverse_payoffs(problem: KnapsackPackingGame) -> np.ndarray:
+    greedy_solution = problem.solve_greedy()
+    i_range = list(range(problem.m))
+
+    model = gp.Model("Local Inverse Weights")
+
+    p = model.addMVar((problem.n, problem.m), vtype=GRB.INTEGER, lb=0)
+    delta = model.addMVar((problem.n, problem.m), lb=0)
+
+    model.setObjective(delta.sum())
+
+    model.addConstrs(
+        delta[j, i] >= p[j, i] - problem.payoffs[j, i]
+        for i in i_range
+        for j in problem.players
+    )
+    model.addConstrs(
+        delta[j, i] >= problem.payoffs[j, i] - p[j, i]
+        for i in i_range
+        for j in problem.players
+    )
+
+    true_values = [
+        greedy_solution[j] @ p[j]
+        + sum(
+            greedy_solution[j] * greedy_solution[o] @ problem.inter_coefs[j, o]
+            for o in problem.opps[j]
+        )
+        for j in problem.players
+    ]
+
+    model.optimize()
+    if model.Status == GRB.INFEASIBLE:
+        raise ValueError("Problem is initially Infeasible")
+
+    # solutions = [set() for _ in problem.players]
+
+    while True:
+        new_constraint = False
+
+        for j in problem.players:
+            new_player_x = problem.solve_player(
+                j, solution=greedy_solution, payoffs=p.X
+            )
+
+            new_value = new_player_x @ p[j] + sum(
+                new_player_x * greedy_solution[o] @ problem.inter_coefs[j, o]
+                for o in problem.opps[j]
+            )
+
+            if new_value.getValue() >= true_values[j].getValue() + eps:
+                model.addConstr(new_value <= true_values[j])
+                new_constraint = True
+
+        if not new_constraint:
+            break
+
+        model.optimize()
+        if model.Status == GRB.INFEASIBLE:
+            raise ValueError("Problem is Infeasible")
+
+    return p.X.astype(int)
