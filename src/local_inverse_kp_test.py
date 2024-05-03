32a1a795de588fad0e031f2a1b4d42500b64ef01
plofknaapje <robinverhoef1007@gmail.com>
2024-05-03 14:05:56 +0200

    Update local_inverse_kp_test.py

--- src/local_inverse_kp_test.py
+++ src/local_inverse_kp_test.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-from time import time
 
 from methods.local_inverse_kp import (
     generate_problem,
@@ -8,29 +6,12 @@ from methods.local_inverse_kp import (
     local_inverse_weights,
 )
 
-rng = np.random.default_rng(42)
-n_items = [100, 500, 1000, 5000, 10000]
-ranges = [1000, 5000, 10000]
-repeats = 30
+problem = generate_problem(50, capacity=0.5)
 
-payoff_data = []
-weight_data = []
-
-for size in n_items:
-    for r in ranges:
-        payoff_results = []
-        for _ in repeats:
-            problem = generate_problem(5000, r=10000, capacity=0.5, rng=rng)
-            greedy_solution = problem.solve_greedy()
-            start = time()
-            payoffs = local_inverse_payoffs(problem)
-            end = time() - start
-            payoff_results.append
+greedy_solution = problem.solve_greedy()
 
 payoffs = local_inverse_payoffs(problem)
 print("Payoffs change:", np.abs(problem.payoffs - payoffs).sum())
-
-
 
 weights = local_inverse_weights(problem)
 print("Weights change:", np.abs(problem.weights - weights).sum())
